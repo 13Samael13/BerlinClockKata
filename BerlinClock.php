262caf7d655e332d652686f472b285c9a12786ff
Diego Gilson <diego.gilson@student.vinci.be>
2021-11-23 09:52:29 +0100

    update

--- BerlinClock.php
+++ BerlinClock.php
@@ -9,4 +9,20 @@
             return "OOOO";
         }
 
+        public function blocDe5Min(string $nbMinutes) : string{
+            if($nbMinutes >= "60") return "00000000000";
+            if($nbMinutes >= "55") return "YYRYYRYYRYY";
+            if($nbMinutes >= "50") return "YYRYYRYYRY0";
+            if($nbMinutes >= "45") return "YYRYYRYYR00";
+            if($nbMinutes >= "40") return "YYRYYRYY000";
+            if($nbMinutes >= "35") return "YYRYYRY0000";
+            if($nbMinutes >= "30") return "YYRYYR00000";
+            if($nbMinutes >= "25") return "YYRYY000000";
+            if($nbMinutes >= "20") return "YYRY0000000";
+            if($nbMinutes >= "15") return "YYR00000000";
+            if($nbMinutes >= "10") return "YY000000000";
+            if($nbMinutes >= "5") return "Y0000000000";
+            return "00000000000";
+        }
+
     }
--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -2,8 +2,18 @@
 
 
 use PHPUnit\Framework\TestCase;
+require 'BerlinClock.php';
 
-class BerlinClockTest extends TestCase
-{
+class BerlinClockTest extends TestCase{
+
+    public function testMinutesSimpleReturn1(){
+        //arrange
+        $BerlinClock = new BerlinClock();
+        // Act
+        $actual = $BerlinClock->minutesSimple();
+        //Assert
+        $this->assertEquals( "1",$actual);
+    }
+
 
 }
