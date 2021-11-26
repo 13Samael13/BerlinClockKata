49bc8917192da9d67c060c0cb3afb10f922ef002
13Samael13 <roman.allion@student.vinci.be>
2021-11-26 13:51:44 +0100

    added secondes method

--- BerlinClock.php
+++ BerlinClock.php
@@ -1,11 +1,11 @@
 <?php
     class BerlinClock{
         //Y --> Yellow R --> Red O --> OFF
-        public function minutesSimple(string $nbrMinutes) : string{
-            if($nbrMinutes%5 == "1") return "YOOO";
-            if($nbrMinutes%5 == "2") return "YYOO";
-            if($nbrMinutes%5 == "3") return "YYYO";
-            if($nbrMinutes%5 == "4") return "YYYY";
+        public function minutesSimple(string $nbMinutes) : string{
+            if($nbMinutes%5 == "1") return "YOOO";
+            if($nbMinutes%5 == "2") return "YYOO";
+            if($nbMinutes%5 == "3") return "YYYO";
+            if($nbMinutes%5 == "4") return "YYYY";
             return "OOOO";
         }
 
@@ -40,5 +40,11 @@
             if($nbHeures >= "5") return "ROOO";
             return "OOOO";
         }
+
+        public function secondes(string $nbSecondes) : string{
+            if($nbSecondes % 2 == "1") return "X";
+            return "R";
+        }
+
 
     }
